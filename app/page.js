3b242b433944f9dccfc17a6d2138938e0b6e92da
Damini Tiwari <95232532+Damini2630@users.noreply.github.com>
2025-06-03 20:25:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,13 +3,29 @@
 import { useDashboard } from "../context/DashboardContext";
 import UserCard from "../components/UserCard";
 import SearchFilter from "../components/SearchFilter";
+import { Typography } from "@mui/material";
 
 export default function HomePage() {
   const { filteredUsers } = useDashboard();
 
   return (
     <div className="p-6 space-y-4">
-      <h1 className="text-3xl font-bold text-black">HR Performance Dashboard</h1>
+      <Typography
+  variant="h3"
+  fontWeight="bold"
+  sx={{
+    color: "primary.main",
+    textTransform: "uppercase",
+    letterSpacing: 1.5,
+    mb: 2,
+    borderBottom: "3px solid",
+    borderColor: "primary.light",
+    display: "inline-block",
+  }}
+>
+  HR Performance Dashboard
+</Typography>
+
       <SearchFilter />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {filteredUsers.map((user) => (
