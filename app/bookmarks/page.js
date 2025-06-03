ecbf55dc5d6ee5d8e53100921856d6f871e25455
Damini Tiwari <95232532+Damini2630@users.noreply.github.com>
2025-06-03 18:42:48 +0530

    Update page.js

--- app/bookmarks/page.js
+++ app/bookmarks/page.js
@@ -100,9 +100,9 @@ export default function BookmarksPage() {
                     {promoted.includes(user.id) ? "Promoted ✅" : "Promote"}
                   </Button>
 
-                  <Button size="small" color="secondary">
-                    Assign to Project
-                  </Button>
+                  // <Button size="small" color="secondary">
+                  //   Assign to Project
+                  // </Button>
                 </Stack>
               </CardActions>
             </Card>
